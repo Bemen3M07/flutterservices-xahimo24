520902ecca1949955e5475b26f4cd3ab49931d44
xahimo24 <xavier.hidalgo.grail@gmail.com>
2025-03-12 19:19:03 +0100

    rubrica update v2

--- lib/car_provider.dart
+++ lib/car_provider.dart
@@ -3,17 +3,17 @@ import 'car_http_service.dart';
 import 'car_model.dart';
 
 class CarProvider with ChangeNotifier {
-  List<CarModel> _cars = [];
+  List<CarsModel> _cars = [];
   bool _isLoading = false;
 
-  List<CarModel> get cars => _cars;
+  List<CarsModel> get cars => _cars;
   bool get isLoading => _isLoading;
 
   Future<void> fetchCars() async {
     _isLoading = true;
     notifyListeners();
 
-    CarService carService = CarService();
+    CarHttpService carService = CarHttpService();
     _cars = await carService.getCars();
 
     _isLoading = false;
